91cfffe353cd627e23763b214ae809609f823025
Elio Molinaro <emolinaro.work@gmail.com>
2023-09-23 20:55:10 +0200

    TEMP- updated batch files to use anaconda path
    Batch files now execute python from the sewers conda env

--- startSewers.bat
+++ startSewers.bat
@@ -6,6 +6,6 @@ REM Changes the current working directory to the directory the bat file is in
 cd /d %~dp0
 
 REM Run __init__.py file through the python exec
-python __init__.py
+"C:\Users\Elio\anaconda3\envs\sewers\python.exe" __init__.py
 
 pause
