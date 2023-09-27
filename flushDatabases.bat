956df82b5e71471df0a46b492ec094dd27328fc7
Elio Molinaro <emolinaro.work@gmail.com>
2023-09-27 22:38:42 +0200

    bug- changed the bat files back to use the regular python interpreter

--- flushDatabases.bat
+++ flushDatabases.bat
@@ -6,6 +6,6 @@ REM Changes the current working directory to the directory the bat file is in
 cd /d %~dp0
 
 REM Run files through the python exec
-"%USERPROFILE%\anaconda3\envs\sewers\python.exe" utils/flushDatabases.py
+python utils/flushDatabases.py
 
 pause
